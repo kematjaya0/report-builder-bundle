f6c87e2414af767a9e9b4fedbb1a7066a7869a99
kematjaya <kematjaya0@gmail.com>
2026-03-10 14:39:43 +0700

    assets

--- src/Twig/ReportMenuExtension.php
+++ src/Twig/ReportMenuExtension.php
@@ -4,6 +4,7 @@ namespace Kematjaya\ReportBuilderBundle\Twig;
 
 use Kematjaya\ReportBuilderBundle\Builder\ReportMenuBuilderInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -13,7 +14,7 @@ use Twig\TwigFunction;
 class ReportMenuExtension extends AbstractExtension
 {
     public function __construct(
-        private ReportMenuBuilderInterface $builder
+        private ReportMenuBuilderInterface $builder, private Environment $twig
     ) {
     }
 
@@ -21,11 +22,29 @@ class ReportMenuExtension extends AbstractExtension
     {
         return [
             new TwigFunction('report_saved_menus', $this->getReportMenus()),
+            new TwigFunction("render_chart_js", [$this, 'renderChartJS'], ['is_safe' => ['html']]),
+            new TwigFunction("render_report_builder_css", [$this, 'renderCSS'], ['is_safe' => ['html']]),
+            new TwigFunction("render_report_builder_js", [$this, 'renderJS'], ['is_safe' => ['html']])
         ];
     }
 
     public function getReportMenus(): array
     {
         return $this->builder->build();
+    }
+
+    public function renderChartJS():string
+    {
+        return $this->twig->render('@ReportBuilder/chart_javascript.twig');
+    }
+
+    public function renderCSS():string
+    {
+        return $this->twig->render('@ReportBuilder/stylesheets.twig');
+    }
+
+    public function renderJS():string
+    {
+        return $this->twig->render('@ReportBuilder/javascripts.twig');
     }
 }
